dbf11a85d5424c68b566ba3ed5c1cfa6ee01f8f0
Daniel Kats <daniel.kats@nortonlifelock.com>
2020-08-19 17:30:59 -0400

    lint server

--- server.js
+++ server.js
@@ -4,46 +4,47 @@ My original implementation was using PHP
 However I find it easier to write in node now and I want to make this repo usable
 */
 
-
 // imports
-const express = require("express");
-const morgan = require("morgan");
-const app = express();
-const knex = require("knex")({
-    client: "sqlite3",
+const express = require('express');
+const morgan = require('morgan');
+const knex = require('knex')({
+    client: 'sqlite3',
     connection: {
-        filename: "./courses.db"
-    }
+        filename: './courses.db',
+    },
 });
-const bookshelf = require("bookshelf")(knex);
+const bookshelf = require('bookshelf')(knex);
 
-const Course = bookshelf.model("Course", {
-    tableName: "courses"
+const app = express();
+
+const Course = bookshelf.model('Course', {
+    tableName: 'courses',
 });
 
-const Offering = bookshelf.model("Offering", {
-    tableName: "timetable"
+const Offering = bookshelf.model('Offering', {
+    tableName: 'timetable',
 });
 
 // globals
 const PORT = 5050;
 
 // app config
-app.use(morgan("dev"));
-app.use("/", express.static("public"))
+app.use(morgan('dev'));
+app.use('/', express.static('public'));
 
 // routes
-app.get("/api/offerings", async (req, res) => {
+app.get('/api/offerings', async (req, res) => {
     const offerings = await Offering.fetchAll();
     res.json(offerings);
 });
 
-app.get("/api/courses", async (req, res) => {
+app.get('/api/courses', async (req, res) => {
     const courses = await Course.fetchAll();
     res.json(courses);
 });
 
 // start the server
 app.listen(PORT, () => {
+    // eslint-disable-next-line
     console.log(`Running on http://localhost:${PORT}`);
 });
